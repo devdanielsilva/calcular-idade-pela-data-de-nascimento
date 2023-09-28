eba6cb37146562b8d64ac96f381db0dcccc78dad
Daniel Silva <devdanielsilva@gmail.com>
2023-09-28 15:38:50 -0300

    .

---
+++ javascript/calculo-da-idade.js
@@ -0,0 +1,23 @@
+/*Vamos primeiramente criar uma função que recebe a data no formato YYYY/MM/DD
+
+Que é o formato universal para datas.
+
+Vamos criar objetos a partir da classe Date, serão dois:
+
+Um com a data atual;
+E o outro com a data baseada na data de aniversário;
+Depois vamos fazer subtrações com os valores, para resgatar a diferença entre anos
+
+Que justamente é a idade final! 🙂*/
+function getAge(dateString){
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+
+    if(m < 0 || (m === 0 && today,getDate() < birthDate.getDate())){
+        age--;
+    }
+    return age;
+}
+print(getAge("2000/09/13"));
